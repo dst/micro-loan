6b3565a69feb2bac90425c3807ca684cbd97f43c
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-06-08 12:28:40 +0200

    Temporary ignore respository tests for faster TDD iterations

--- src/test/java/com/stefanski/loan/core/repository/CustomerRepositoryIntegrationTest.java
+++ src/test/java/com/stefanski/loan/core/repository/CustomerRepositoryIntegrationTest.java
@@ -2,6 +2,7 @@ package com.stefanski.loan.core.repository;
 
 import com.stefanski.loan.Application;
 import com.stefanski.loan.core.domain.Customer;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static com.stefanski.loan.util.TestDataFixture.simpleCustomer;
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * @author Dariusz Stefanski
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class)
+@Ignore("Too slow, ignore now for faster TDD")
 public class CustomerRepositoryIntegrationTest {
 
     @Autowired
