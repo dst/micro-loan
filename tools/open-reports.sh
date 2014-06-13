a66bd76a0d9091990e7c45b479aadf1bdf846cd1
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-06-13 12:18:49 +0200

    Fix for  openning reports

--- tools/open-reports.sh
+++ tools/open-reports.sh
@@ -5,7 +5,7 @@
 # Author: Dariusz Stefanski
 # Date:   11.06.2014
 
-REPORTS_DIR="../build/reports/"
+REPORTS_DIR=`pwd`/"../build/reports"
 BROWSER="firefox"
 
 for report in /jacoco/test/html/index.html /tests/index.html /cucumber/index.html; do
