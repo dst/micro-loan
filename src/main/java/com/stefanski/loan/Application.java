e0bd0af6aaa1a9589e1d7cd3217f1c90ad823489
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-06-15 23:40:14 +0200

    Swagger custom configuration

--- src/main/java/com/stefanski/loan/Application.java
+++ src/main/java/com/stefanski/loan/Application.java
@@ -1,6 +1,5 @@
 package com.stefanski.loan;
 
-import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * @author Dariusz Stefanski
  */
-@EnableSwagger
 @EnableAutoConfiguration
 @ComponentScan("com.stefanski.loan")
 public class Application {
---
+++ src/main/java/com/stefanski/loan/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.stefanski.loan;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.wordnik.swagger.model.ApiInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Dariusz Stefanski
+ */
+@Configuration
+@EnableSwagger
+public class SwaggerConfig {
+
+    @Autowired
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                .apiInfo(apiInfo())
+                .includePatterns("/loans.*", "/customers.*", "/info");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "MICRO-LOAN",
+                "RESTful web service for micro loans",
+                "http://www.keepcalm-o-matic.co.uk/p/keep-calm-and-do-whatever-you-want-4",
+                "you-can-find-yourself@example.com",
+                "Apache License, Version 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0.html"
+        );
+    }
+}
