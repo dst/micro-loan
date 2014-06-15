7ab082e4ea866e55206f9d29e26f816ae98a150f
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-06-15 19:48:35 +0200

    Swagger documentation

--- src/main/java/com/stefanski/loan/Application.java
+++ src/main/java/com/stefanski/loan/Application.java
@@ -1,5 +1,6 @@
 package com.stefanski.loan;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * @author Dariusz Stefanski
  */
+@EnableSwagger
 @EnableAutoConfiguration
 @ComponentScan("com.stefanski.loan")
 public class Application {
