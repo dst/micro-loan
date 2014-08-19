085a254bc06c0d2ed004228399168710d5a9a706
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-08-19 23:20:08 +0200

    Fixed typo in javadoc

--- src/main/java/com/stefanski/loan/core/repository/LoanRepositoryCustom.java
+++ src/main/java/com/stefanski/loan/core/repository/LoanRepositoryCustom.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public interface LoanRepositoryCustom {
 
     /**
-     * Get number of loans which where request in a given day from a give IP address.
+     * Get number of loans which where requested in a given day from a give IP address.
      *
      * @param ip  IP address from where loan was taken
      * @param day when loan was created
