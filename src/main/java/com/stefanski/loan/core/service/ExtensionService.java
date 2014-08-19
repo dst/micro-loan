1e235fb4b67cd8480d25def25d4cba38d56c1149
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-08-20 00:00:02 +0200

    Split ExtensionService into smaller methods

--- src/main/java/com/stefanski/loan/core/service/ExtensionService.java
+++ src/main/java/com/stefanski/loan/core/service/ExtensionService.java
@@ -39,37 +39,50 @@ public class ExtensionService {
     }
 
     public Extension findLoanExtension(Long loanId, Long extensionId) {
-        Extension ext = extensionRepository.findOne(extensionId);
-        if (ext == null) {
-            String msg = String.format("Extension with id %d does not exist", extensionId);
-            throw new ResourceNotFoundException(msg);
-        }
-
-        if (!loanId.equals(ext.getLoan().getId())) {
-            String msg = String.format("Extension with id %d which belongs to loan %d does not exist",
-                    extensionId, loanId);
-            throw new ResourceNotFoundException(msg);
-        }
-
+        Extension ext = getExtension(extensionId);
+        checkThatExtBelongsToLoan(ext, loanId);
         log.debug("Found extension: {}", ext);
         return ext;
     }
 
     public Long extendLoan(Long loanId) {
         Loan loan = loanService.findLoanById(loanId);
+        Extension extension = createExtensionFor(loan);
+        extendLoan(loan);
+        return extension.getId();
+    }
 
+    private Extension getExtension(Long extensionId) {
+        Extension ext = extensionRepository.findOne(extensionId);
+        if (ext == null) {
+            String msg = String.format("Extension with id %d does not exist", extensionId);
+            throw new ResourceNotFoundException(msg);
+        }
+        return ext;
+    }
+
+    private void checkThatExtBelongsToLoan(Extension ext, Long loanId) {
+        if (!loanId.equals(ext.getLoan().getId())) {
+            String msg = String.format("Extension with id %d which belongs to loan %d does not exist",
+                    ext.getId(), loanId);
+            throw new ResourceNotFoundException(msg);
+        }
+    }
+
+    private Extension createExtensionFor(Loan loan) {
         Extension extension = new Extension();
         extension.setCreationTime(LocalDateTime.now());
         extension.setLoan(loan);
         extension = extensionRepository.save(extension);
         log.info("Created extension: {}", extension);
+        return extension;
+    }
 
+    private void extendLoan(Loan loan) {
         loan.extendDeadline(getExtensionDays());
         loan.multipleInterest(getExtensionInterest());
         loanService.save(loan);
         log.info("Updated loan: {}", loan);
-
-        return extension.getId();
     }
 
     private int getExtensionDays() {
